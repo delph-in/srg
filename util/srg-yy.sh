fed2ad9f02fdf66099f8b3b6ee44c5abe296450f
Olga Zamaraeva <olzama@uw.edu>
2022-07-27 01:08:37 +0200

    Update srg-yy.sh
    Expanded usage documentation for the freeling -> yy-input script.

--- util/srg-yy.sh
+++ util/srg-yy.sh
@@ -2,7 +2,11 @@
 # Expected usage should be something like:
 # 
 # $ bash srg-yy.sh srg-test.txt
-# (where "srg-test.txt" is a text file with one sentence in each line)
+#
+# ...where "srg-test.txt" is a text file with one sentence in each line.
+# Make sure you have the correct path to your srg.dat (or whatever your ACE-compiled
+# grammar is called). You may need to modify the python command to python3,
+# depending on your environment.
 ################################################################################
 
 MYPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
