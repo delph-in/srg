579c4bde92e0f1f7ca6ebfaa570abc06b7372fe9
olzama <olga.zamaraeva@gmail.com>
2022-08-25 11:48:49 +0200

    successfully parse with yy-mode with ace-0.9.34

--- util/ace_wrapper.py
+++ util/ace_wrapper.py
@@ -4,7 +4,9 @@ from delphin import ace
 with open(sys.argv[1], 'r') as f:
     sentence_list = f.readlines()
 grammar = sys.argv[2]
+ace_exec = sys.argv[3]
 ace_responces = []
-for s in sentence_list:
-    response = ace.parse(grammar, s)
-    ace_responces.add(response)
+with ace.ACEParser(grammar,cmdargs=['-y','--yy-rules'],executable=ace_exec) as parser:
+    for s in sentence_list:
+        response = parser.interact(s)
+        ace_responces.append(response)
