abd404077b80c141b8f21027c0ac05acb2255fb6
Olga Zamaraeva <olzama@uw.edu>
2022-09-05 16:40:35 +0200

    find matching tags with confidence computed based on set intersection between corresponding surface forms

--- util/compare_freeling.py
+++ util/compare_freeling.py
@@ -1,6 +1,40 @@
 import sys
 from pathlib import Path
 
+TAGS = 'tags'
+SURFACES = 'surfaces'
+
+def process_freeling_file(f, data):
+    k = f.suffix[1:]
+    data[k] = {TAGS:{}, SURFACES:{}}
+    with open(f, 'r') as old_v:
+        lines = old_v.readlines()
+    for ln in lines:
+        surface, lemma, tag = ln.split()
+        if not surface in data[k][SURFACES]:
+            data[k][SURFACES][surface] = []
+        data[k][SURFACES][surface].append(tag)
+        if not tag in data[k][TAGS]:
+            data[k][TAGS][tag] = []
+        data[k][TAGS][tag].append(surface)
+    return data
+
+def find_match():
+    forms = new_version[pos][TAGS][tag]
+    for form in forms:
+        if form in old_version[pos][SURFACES]:
+            old_tags = old_version[pos][SURFACES][form]
+            for ot in old_tags:
+                oforms = old_version[pos][TAGS][ot]
+                isect = len(set(forms).intersection(oforms))/len(set(forms))
+                if isect > 0.5:
+                    return ot, isect
+        else:
+            print('Form {} not found'.format(form))
+
+
+old_version = {}
+new_version = {}
 
 old_freeling_dict_files = sorted(Path(sys.argv[1]).glob('**/MM.*'))
 new_freeling_dict_files = sorted(Path(sys.argv[2]).glob('**/MM.*'))
@@ -10,11 +44,26 @@ new_names = [f.name for f in new_freeling_dict_files]
 
 assert old_names == new_names
 
-for freeling_dict_file in Path(sys.argv[1]).glob('**/MM.*'):
-    with open(freeling_dict_file, 'r') as old_v:
-        old_v_lines = old_v.readlines()
 
-with open(sys.argv[2], 'r') as new_v:
-    new_v_lines = new_v.readlines()
+for dict_file in old_freeling_dict_files:
+    process_freeling_file(dict_file, old_version)
 
-print(5)
+for dict_file in new_freeling_dict_files:
+    process_freeling_file(dict_file, new_version)
+
+to_update = []
+
+
+
+for pos in new_version:
+    print(pos)
+    for tag in new_version[pos][TAGS]:
+        if not tag in old_version[pos][TAGS]:
+            matching_tag, confidence = find_match()
+            to_update.append('{}->{} with confidence {}'.format(matching_tag, tag, confidence))
+
+print(len(to_update))
+
+with open('to_update', 'w') as f:
+    for tag in to_update:
+        f.write(tag + '\n')
