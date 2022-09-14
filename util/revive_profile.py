e4f6506c2177b786c007d4e91e11f1e780961328
Olga Zamaraeva <olzama@uw.edu>
2022-09-14 16:14:18 +0200

    Check in the mrs test suite text file and the script which I used to retrieve it from an old tsdb profile.

--- util/revive_profile.py
+++ util/revive_profile.py
@@ -1,4 +1,17 @@
 import sys
 from delphin import ace, itsdb
+import glob
+
+def read_testsuite(path):
+    ts = itsdb.TestSuite(path)
+    items = list(ts.processed_items())
+    sentences = [item['i-input'] for item in items]
+    return sentences
+
 
 testsuite_path = sys.argv[1]
+
+sentences = read_testsuite(testsuite_path)
+with open('srg-mrs.txt','w') as f:
+    for s in sentences:
+        f.write(s + '\n')
