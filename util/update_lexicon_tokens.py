fb1568288cf50df505cdaff74b156d1a4916ea09
Olga Zamaraeva <olzama@uw.edu>
2022-11-22 15:05:36 +0100

    Script to update the lexicon for native token constraints.

--- util/update_lexicon_tokens.py
+++ util/update_lexicon_tokens.py
@@ -1,16 +1,25 @@
 import glob, sys
 
-from delphin import tdl
+#from delphin import tdl
+from gmcs import tdl
 
 
-def parse_lexicons(lexicons):
-    for lexicon in glob.iglob(lexicons + '**'):
-        for event, obj, lineno in tdl.iterparse(lexicon):
-            if event == 'TypeDefinition':
-                new_supertype = tdl.TypeIdentifier('native_le')
-                obj.supertypes.append(tdl.TypeIdentifier('native_le'))
-                print(5)
-
+def parse_lexicons(filepath):
+    with open(filepath, 'r') as f:
+        lexicon_text = f.read()
+    entries = lexicon_text.split('\n\n') # Assume that the entries in the lexicon are separated by two newlines
+    updated_lexicon = tdl.TDLfile('lexicon-updated.tdl')
+    for e in entries:
+        print(e)
+        if e.strip():
+            e_norm = e.strip() # remove any remaining trailing newlines
+            if not e_norm.startswith(';'):
+                updated_lexicon.add(e_norm)
+                type_name = e_norm.split(':=')[0].strip()
+                updated_lexicon.add(type_name + ':= [ TRAITS native_token_list ].',merge=True)
+            else:
+                updated_lexicon.add_literal(e_norm)
+    updated_lexicon.save()
 
 
 parse_lexicons(sys.argv[1])
