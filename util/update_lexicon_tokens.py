7795fe360722c8a9466e81c02d119cafb686d3cc
Olga Zamaraeva <olzama@uw.edu>
2022-11-23 15:20:29 +0100

    I think this script correctly updates the letypes file and the lexicon file to include a native_le type and to have all lexicon entries inherit from it. I have not yet tried to compile this though.

--- util/update_lexicon_tokens.py
+++ util/update_lexicon_tokens.py
@@ -18,7 +18,7 @@ def update_lexicon(filepath_lex, filepath_letypes):
             #print(pydelphin_tdl.format(obj))
             if obj.identifier in le_to_update:
                 print(obj.identifier)
-                new_id = pydelphin_tdl.TypeIdentifier(obj.identifier + '_native')
+                new_id = add_word_to_typename(obj.identifier, '_native', '_le')
                 ds = '' if not obj.docstring else obj.docstring
                 conj = copy.deepcopy(obj.conjunction)
                 new_type = pydelphin_tdl.TypeDefinition(new_id, conj, ds +
@@ -27,28 +27,40 @@ def update_lexicon(filepath_lex, filepath_letypes):
                 new_type.conjunction.add(pydelphin_tdl.TypeIdentifier('native_le'))
                 updated_letypes.append(new_type)
             updated_letypes.append(obj)
-    with open('updated_letypes.tdl', 'w') as f:
-        for obj in updated_letypes:
-            f.write(pydelphin_tdl.format(obj) + '\n\n')
+        else:
+            updated_letypes.append(obj)
+    save_tdl_objects(updated_letypes, 'updated_letypes.tdl')
+
+    updated_lexical_entries = []
+    for event, obj, lineno in pydelphin_tdl.iterparse(filepath_lex):
+        if event == 'TypeDefinition':
+            terms = [add_word_to_typename(obj.identifier, '_native', '_le')]
+            terms.extend(obj.conjunction.terms[1:])
+            conj = pydelphin_tdl.Conjunction(terms)
+            new_type = pydelphin_tdl.TypeDefinition(obj.identifier, conj, obj.docstring)
+            updated_lexical_entries.append(new_type)
+        else:
+            updated_lexical_entries.append(obj)
+
+    save_tdl_objects(updated_lexical_entries, 'updated_lexicon.tdl')
 
 
+def save_tdl_objects(tdl_objects, filename):
+    with open(filename, 'w') as f:
+        for obj in tdl_objects:
+            if isinstance(obj, str):
+                if not obj.startswith(';'):
+                    f.write('; ' + obj + '\n\n')
+                else:
+                    f.write(obj + '\n\n')
+            else:
+                f.write(pydelphin_tdl.format(obj) + '\n\n')
 
-    # with open(filepath_lex, 'r') as f:
-    #     lexicon_text = f.read()
-    # with open(filepath_letypes, 'r') as f:
-    #     letypes_text = f.read()
-    # entries = lexicon_text.split('\n\n') # Assume that the entries in the lexicon are separated by two newlines
-    # updated_lexicon = matrix_tdl.TDLfile('lexicon-updated.tdl')
-    # updated_letypes = matrix_tdl.TDLfile('letypes-updated.tdl')
-    # for e in entries:
-    #     print(e)
-    #     if e.strip():
-    #         e_norm = e.strip() # remove any remaining trailing newlines
-    #         if not e_norm.startswith(';'):
-    #             updated_lexicon.add(e_norm)
-    #             supertype_name = e_norm.split(':=')[1].strip().split('&')[0].strip()
-    #         else:
-    #             updated_lexicon.add_literal(e_norm)
-    # updated_lexicon.save()
+# print(pydelphin_tdl.format(obj))
+
+
+def add_word_to_typename(name, word, suffix):
+    first_part = name[:len(name)-len(suffix)]
+    return pydelphin_tdl.TypeIdentifier(first_part + word + suffix)
 
 update_lexicon(sys.argv[1],sys.argv[2])
