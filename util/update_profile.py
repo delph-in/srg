fa055363ad378fb3cdb4336d30dbdc1aa8b088fb
Olga Zamaraeva <olzama@uw.edu>
2022-12-13 15:32:03 +0100

    Changed the script to work from command line and added a usage comment.

--- util/update_profile.py
+++ util/update_profile.py
@@ -1,3 +1,28 @@
+'''
+This script helps update outdated [incr tsdb()] profiles, which miss some columns in some files.
+To add new empty columns, for example, to turn
+
+1@@gcc 3.4@1@2.0 (23-jun-13; beta)@PET(tom cheap v0.99.14svn_cm) [... more things here ] @complete
+
+into
+
+1@@gcc 3.4@1@2.0 (23-jun-13; beta)@@PET(tom cheap v0.99.14svn_cm) [... more things here ] @complete
+
+...in the file called "run", you need to run this script as follows:
+
+python3 update_profile.py file-to-testuite-folder/run 4
+
+This means insert an @ sign into the fourth position, as the fourth column.
+
+You can also use the script to insert more than one @ at the same time, but be careful:
+if you need to insert several @ signs in a row, you need to ask for them to be inserted in the same position,
+e.g.: python3 update_profile.py path-to-testsuite-folder/item 8,8,8. If the positions are not consecutive,
+use the actual positions, e.g. 2,5
+
+'''
+
+
+import sys
 
 def find_all(s, ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
@@ -18,8 +43,13 @@ def insert_at(filepath, positions):
             f.write(updated_item)
 
 
-insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/run',[4])
-insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/parse',[24,24,24,24,24])
-insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/edge',[8])
-insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/score',[3,3,3,3])
-insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/item',[8,8,8])
+fp = sys.argv[1]
+positions = [ int(n) for n in sys.argv[2].split(',') ]
+
+insert_at(fp, positions)
+
+# insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/run',[4])
+# insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/parse',[24,24,24,24,24])
+# insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/edge',[8])
+# insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/score',[3,3,3,3])
+# insert_at('/Users/olzama/Research/delphin/Marimon/updated-tsdb/mrs/item',[8,8,8])
