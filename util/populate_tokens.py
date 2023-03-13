dc0ece5100190a438f549aa4cbd0644650e4b4f3
olzama <olga.zamaraeva@gmail.com>
2023-03-13 13:00:30 +0100

    in progress: include original character positions in freeling output

--- util/populate_tokens.py
+++ util/populate_tokens.py
@@ -2,9 +2,12 @@ import sys
 import string
 import glob
 import subprocess
+import json
 from tempfile import NamedTemporaryFile
 from delphin import itsdb
-from srg_freeling2yy import convert_sentences
+from srg_freeling2yy import convert_sentences_json
+# I cannot figure out how to use the pyfreeling library:
+# from pyfreeling import Analyzer
 
 # REMOVE = {'The tobacco garden dog barked.', 'Abrams wiped the table clean.',
 #           'Abrams left it to Browne to bark.', 'How happy was Abrams?'}
@@ -13,8 +16,8 @@ def read_testsuite(ts):
     items = ts['item']
     # Strip the trailing hyphens to match old LKB output, although may want to put them back in later.
     sentences = [item['i-input'].strip('-') for item in items ]
-    # debug
-    #sentences = [ sentences[59] ]
+    # debug:
+    #sentences = sentences[47:49]
     tmp_sentence_file = NamedTemporaryFile("w", delete=False)
     with open(tmp_sentence_file.name, 'w') as tmp_f:
         for s in sentences:
@@ -27,16 +30,53 @@ def read_testsuite(ts):
 def run_script(script_path, arg_path):
     output = subprocess.run("'%s' '%s'" % (script_path, str(arg_path)),shell=True,stdout=subprocess.PIPE)
     #print(output.stdout.decode('utf-8'))
-    return [s for s in output.stdout.decode('utf-8').split('\n\n') if s]
+    decoded_output = output.stdout.decode('utf-8')
+    json_strs = parse_recursive_dicts(decoded_output)
+    return json_strs
+
+'''
+Code produced by 
+ChatGPT
+'''
+def parse_recursive_dicts(input_string):
+    sentences = []
+    start = 0
+    end = 0
+
+    while True:
+        start = input_string.find('{', end)
+        if start == -1:
+            break
+        #print(input_string[start],end='')
+        end = start + 1
+        depth = 1
+        while depth > 0:
+            #print(input_string[end],end='')
+            if input_string[end] == '{':
+                depth += 1
+            elif input_string[end] == '}':
+                depth -= 1
+            end += 1
+        sentence_str = input_string[start:end]
+        sentence = json.loads(sentence_str)
+        sentences.append(sentence)
+        #print('\n')
+    return sentences
 
 def update_testsuite(ts):
+    # analyzer = pyfreeling.Analyzer()
+    # analyzer.lang = 'es'
+    # analyzer.config = 'es.cfg'
+    #scr_out_xml = analyzer.run(sentence_list)
     sentence_list = read_testsuite(ts)
-    script_output = run_script('./sentences2freeling.sh', sentence_list)
-    yy = convert_sentences(script_output)
+    scr_out_json = run_script('./sentences2freeling.sh', sentence_list)
+    yy = convert_sentences_json(scr_out_json)
     assert len(yy) == len(ts['item'])
     print('{} items in the corpus'.format(len(yy)))
     for i, row in enumerate(ts['item']):
         ts['item'].update(i, {'i-tokens': yy[i]})
+        if ts['item'][i]['i-id'] == 10:
+            ts['item'].update(i, {'i-id':101})
     ts.commit()
 
 
