828bf68613d7c625a1443f8406ce25f32417b55f
olzama <olga.zamaraeva@gmail.com>
2023-03-16 13:21:46 +0100

    reorganize files under util

---
+++ util/analyze-wrappers/sentences2freeling_json.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+################################################################################
+# Expected usage should be something like:
+# 
+# $ bash sentences2freeling.sh sentences.txt
+#
+# ...where "sentences.txt" is a text file with one sentence in each line.
+# You may need to modify the python command to python3,
+# depending on your environment.
+################################################################################
+
+MYPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+analyze -f es.cfg --output json <$1 2>/dev/null
