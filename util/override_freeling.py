6daebc5e43effb5401972380bb5572471dce590d
olzama <olga.zamaraeva@gmail.com>
2023-04-05 13:38:55 +0200

    changes for release 0.3.0

--- util/override_freeling.py
+++ util/override_freeling.py
@@ -2,10 +2,6 @@
 # Freeling tags to override and replace by other tags
 TAGS = {'I': 'AQ0MS00'}
 
-# Sometimes Freeling gives a wrong tag in very simple cases, such as for the 3SG verb 'ladrar' it returns a noun tag
-# with the probability 78%.
-#LEMMA_TAG_PAIRS = {'NCFS000' : {'ladra': {'prob': 0.80, 'replace': 'VMIP3S0'}}}
-
 REPLACE_LEMMA_AND_TAG = {'ladra': {'lemma': 'ladrar', 'tag':'VMIP3S0'}, 'dió': {'lemma': 'dar', 'tag': 'VMIS3S0'}}
 
 DO_NOT_OVERRIDE = {'uf', 'je', 'ja'}
