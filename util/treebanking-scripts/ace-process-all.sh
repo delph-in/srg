c6e4b99d76d737bce0a0a8fe5ff985c7ea2498bd
olzama <olga.zamaraeva@gmail.com>
2023-04-28 17:55:52 +0200

    add an option to the ace script to report correct number of parsed sentences.

--- util/treebanking-scripts/ace-process-all.sh
+++ util/treebanking-scripts/ace-process-all.sh
@@ -7,6 +7,6 @@ directory="$1"
 
 for profile in "$directory"/*; do
   echo $profile
-  delphin process --options="-y --yy-rules" -g ~/delphin/SRG/grammar/srg/ace/srg.dat --full-forest --select i-tokens "$profile"
+  delphin process --options="-y --yy-rules -1" -g ~/delphin/SRG/grammar/srg/ace/srg.dat --full-forest --select i-tokens "$profile"
 done
 
