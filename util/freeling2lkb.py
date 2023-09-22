19598f2739ecd72864624d64c16e3faadea8e688
Olga Zamaraeva <olzama@uw.edu>
2023-09-22 12:28:28 +0200

    Update the interface to use sppp.dat Freeling override options.

--- util/freeling2lkb.py
+++ util/freeling2lkb.py
@@ -1,9 +1,13 @@
 import sys
 from tokenize_and_tag import Freeling_tok_tagger
 from srg_freeling2yy import convert_sentences
+import parse_sppp_dat
 
 
 if __name__ == "__main__":
+    fuse, replace, no_disambiguate, output = parse_sppp_dat.parse_sppp('./freeling_api/srg-freeling.dat')
+    override_dicts = {'fuse': fuse, 'replace': replace, 'no_disambiguate': no_disambiguate, 'output': output}
+
     # read input from file or standard input, one sentence per line. Put each
     # sentence through FreeLing and convert the output to YY input format.
     ftok = Freeling_tok_tagger()
@@ -15,7 +19,7 @@ if __name__ == "__main__":
         if sent.strip() == "": # FreeLing doesn't cope well with empty input
             print("")
         else:
-            freeling_s = ftok.tokenize_and_tag([sent])
-            print(convert_sentences([freeling_s[0]])[0])
+            freeling_s = ftok.tokenize_and_tag([sent], override_dicts)
+            print(convert_sentences([freeling_s[0]],override_dicts)[0])
     if f is not sys.stdin:
         f.close()
