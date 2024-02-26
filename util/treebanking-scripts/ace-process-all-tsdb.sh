8a935e2bf8012db633c4fe265b77f3fb948bf643
olzama <olga.zamaraeva@gmail.com>
2024-02-26 12:57:37 +0100

    adding ACE scripts for tsdb++

---
+++ util/treebanking-scripts/ace-process-all-tsdb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run ACE on every treebank in a directory, parsing the yy-tokens field instead of the raw sentence string.
+# This means, the sentence was preprocessed such that it was tokenized and potentially POS-tagged.
+
+directory="$1"
+
+for profile in "$directory"/*; do
+  echo $profile
+  delphin process --options="-y --yy-rules --max-chart-megabytes=24000 --max-unpack-megabytes=36000 --timeout=20" -g ~/delphin/SRG/grammar/srg/ace/srg.dat --select i-tokens "$profile"
+done
+
